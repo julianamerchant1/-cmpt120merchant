82b4857841a684adf6357c9839632662ad115526
juliana <juliana.merchant1@marist.edu>
2018-09-11 17:54:04 -0400

    Solution to Mad Libs exercise.

---
+++ madlib.py
@@ -0,0 +1,9 @@
+#Madlib
+def main():
+    noun = input("Send a noun ")
+    adjective = input("Send an adjective ")
+    verb = input("Send a verb ")
+    place = input("Send a place (that can follow 'at') ")
+    print("The", adjective, noun, "likes to", verb, "at",place)
+main()
+      
