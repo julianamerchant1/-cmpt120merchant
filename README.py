a8ba5b65032a822a2131f10382468ba9bb54efc7
juliana <juliana.merchant1@marist.edu>
2018-09-11 17:34:46 -0400

    Revert "Update README.md"
    This reverts commit 8869837572fc9593d7b18996e92a4576a0829aad.

---
+++ README.py
@@ -0,0 +1,10 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 26 2018, 23:26:24) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> # Introduction to Programming
+# Author: Juliana Merchant
+# Date: 9/1/18
+def main():
+print(“Hello, professor!”)
+print(“Good-bye!”)
+main()
---
+++ evens.py
@@ -0,0 +1,3 @@
+for i in range(1):
+	evens = 0,2,4,6,8
+	print evens
