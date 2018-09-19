dd4954a07d71b440a4b16144c519ec9ca708c1de
Juan Arias <juan.arias@marist.edu>
2018-09-18 21:02:33 -0400

    Feedback

--- evens.py
+++ evens.py
@@ -1,4 +1,10 @@
-for i in range(1):
-	evens = 0,2,4,6,8
-	print evens
+# JA: Your version does not run. See below:
+def main():
+        for i in range(2,21,2):
+                print(i)
+
+main()
+#for i in range(1):
+#	evens = 0,2,4,6,8
+#	print evens
 	
