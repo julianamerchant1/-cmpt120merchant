51bad5ec0b0aa5861628f1d057a7525519919278
julianamerchant1 <42786548+julianamerchant1@users.noreply.github.com>
2018-09-25 17:54:10 -0400

    Update lab 5.py

--- lab 5.py
+++ lab 5.py
@@ -11,7 +11,7 @@ def main():
     print(uname)
 # ask user to create a new password
     passwd = input("Create a new password, 8 characters or more: ")
-# TODO modify this to ensure the password has at least 8 characters
+# modify this to ensure the password has at least 8 characters
     while len(passwd)<8:
         print("Fool of a Took! That password is feeble!")
         passwd = input("Create a new password: ")
