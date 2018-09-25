e1c308810946326dbd5ce429ca603246641723a0
julianamerchant1 <42786548+julianamerchant1@users.noreply.github.com>
2018-09-25 17:52:12 -0400

    marist style username 8 character pw
    using while loop

---
+++ lab 5.py
@@ -0,0 +1,20 @@
+# CMPT 120 Intro to Programming
+# Lab #5 – Working with Strings and Functions
+# Author: Juliana Merchant
+# Created: 2000/11/25
+def main():
+# get user's first and last names
+    first = input("Enter your first name: ")
+    last = input("Enter your last name: ")
+# generate a Marist-style username
+    uname = first + '.' + last
+    print(uname)
+# ask user to create a new password
+    passwd = input("Create a new password, 8 characters or more: ")
+# TODO modify this to ensure the password has at least 8 characters
+    while len(passwd)<8:
+        print("Fool of a Took! That password is feeble!")
+        passwd = input("Create a new password: ")
+    print("The force is strong in this one…")
+    print("Account configured. Your new email address is", uname + "@marist.edu")
+main()
