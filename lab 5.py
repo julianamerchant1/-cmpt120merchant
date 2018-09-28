7c53c9b628d40508ffda5bd6a1c66f947758ada0
julianamerchant1 <42786548+julianamerchant1@users.noreply.github.com>
2018-09-28 14:09:52 -0400

    Lab 5 modified with loops
    second part of lab

--- lab 5.py
+++ lab 5.py
@@ -1,7 +1,7 @@
 # CMPT 120 Intro to Programming
 # Lab #5 – Working with Strings and Functions
 # Author: Juliana Merchant
-# Created: 2018/11/25
+# Created: 2000/11/25
 def main():
 # get user's first and last names
     first = input("Enter your first name: ")
@@ -11,10 +11,12 @@ def main():
     print(uname)
 # ask user to create a new password
     passwd = input("Create a new password, 8 characters or more: ")
-# modify this to ensure the password has at least 8 characters
+# ensure the password has at least 8 characters
     while len(passwd)<8:
         print("Fool of a Took! That password is feeble!")
         passwd = input("Create a new password: ")
-    print("The force is strong in this one…")
-    print("Account configured. Your new email address is", uname + "@marist.edu")
+    while len(passwd)>8:
+        print("The force is strong in this one…")
+        print("Account configured. Your new email address is", uname + "@marist.edu")
+        break
 main()
