29942204a4df40edbf89cf4272dc99ab5acc3a4f
julianamerchant1 <42786548+julianamerchant1@users.noreply.github.com>
2018-10-05 23:59:37 -0400

    Add files via upload
    Calc Project Progress. Missing last step for output. Will be seeking help in class.

---
+++ calc.py
@@ -0,0 +1,50 @@
+# CMPT 120 Intro to Programming
+# Calculator Project
+# Author: Juliana Merchant
+# Created: 2018/3/10
+
+def main():
+#take user input for equation
+    equation=input("Input an equation: ")
+    #Split equation to do operations
+    list = equation.split(' ')
+    while len(list)>1:
+        for i in range (1, len(list), 2):
+            #Mult and Div
+            if '*' in list or '/' in list:
+                #Mult
+                if list[i]=='*':
+                    result = float(list[i-1])*float(list[i+1])
+                    del list [i-1:1+2]
+                    list.insert (i-1, result)
+                    break
+                #Div
+                elif list[i] == '/':
+                    result = float(list[i-1])/float(list[i+1])
+                    del list [i-1:1+2]
+                    list.insert (i-1, result)
+                    break
+                #Add and Sub
+            if '+' in list or '-' in list:
+                #Add
+                if list[i] == '+':
+                    result = float(list[i-1])+float(list[i+1])
+                    del list [i-1:1+2]
+                    list.insert (i-1, result)
+                    break
+            #Sub
+            elif list[i] == '-':
+                result = float(list[i-1])-float(list[i+1])
+                del list [i-1:1+2]
+                list.insert (i-1, result)
+                break
+            #else option
+            else:
+                print("error")
+
+#output answer
+  #?????
+  #return result ?
+  #print result ?
+
+main()
