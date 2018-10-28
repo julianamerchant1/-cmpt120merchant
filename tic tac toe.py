a8051992c1d274f8edc91ec425fe1d256c9734e0
Juan Arias <juan.arias@marist.edu>
2018-10-28 13:48:45 -0400

    Feedback

--- tic tac toe.py
+++ tic tac toe.py
@@ -29,19 +29,31 @@ def printBoard(board):
 def markBoard(board, row, col, player):
 # check to see whether the desired square is blank
 # if so, set it to the player number
-    pass
+    #pass
+    if board[row][col] == 0:
+        board[row][col] = player
+        
 def getPlayerMove():
-    input("Input X or O [___] :") # prompt the user separately for the row and column numbers
-    return (0,0) # then return that row and column instead of (0,0)
+    #input("Input X or O [___] :") # prompt the user separately for the row and column numbers
+    #return (0,0) # then return that row and column instead of (0,0)
+    row = int(input("Enter row number: "))
+    col = int(input("Enter column number: "))
+    return row, col
+
 def hasBlanks(board):
     # for each row in the board...
     # for each square in the row...
     # check whether the square is blank
     # if so, return True
-    if []: #not working, should this be a 'for i in' statement?
-        return True
-    else:
-        return False # if no square is blank, return False
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == 0:
+                return True
+    return False
+#    if []: #not working, should this be a 'for i in' statement?
+#        return True
+#    else:
+#        return False # if no square is blank, return False
 def main():
     board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
     player = 1
